b4d48277848a231535c4c18043c6495f254b81ba
Javier Gil <javiergilmont@gmail.com>
2025-02-11 15:41:45 +0100

    refactor: Run server

---
+++ cmd/bootstrap/app.go
@@ -0,0 +1,26 @@
+package bootstrap
+
+import (
+	"github.com/mclargo/go-framework/internal/handlers"
+
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+)
+
+func startApp(bootstrap Bootstrap) *fiber.App {
+	// initialize a new Fiber app
+	app := fiber.New()
+
+	// set endpoint -> healthz
+	app.Get("/healthz", handlers.Healthz)
+
+	if *bootstrap.Config.App.Verbose {
+		for _, route := range app.GetRoutes() {
+			bootstrap.Logger.Debug("Endpoint registered",
+				zap.String("Route", route.Path),
+				zap.String("Method", route.Method),
+			)
+		}
+	}
+	return app
+}
--- cmd/bootstrap/server.go
+++ cmd/bootstrap/server.go
@@ -5,8 +5,6 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/mclargo/go-framework/internal/handlers"
-
 	"github.com/fsnotify/fsnotify"
 	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/viper"
@@ -20,85 +18,24 @@ func RunServer() error {
 		return err
 	}
 
-	// initialize a new Fiber app
-	app := fiber.New()
+	// start the app
+	app := startApp(bootstrap)
 
-	// set endpoint -> healthz
-	app.Get("/healthz", handlers.Healthz)
-
-	if *bootstrap.Config.App.Verbose {
-		for _, route := range app.GetRoutes() {
-			bootstrap.Logger.Debug("Endpoint registered",
-				zap.String("Route", route.Path),
-				zap.String("Method", route.Method),
-			)
-		}
-	}
-
-	return startServer(app, bootstrap)
+	// start the server
+	return runServer(app, bootstrap)
 }
 
-func startServer(app *fiber.App, bootstrap Bootstrap) error {
+func runServer(app *fiber.App, bootstrap Bootstrap) error {
+	// if debug mode, set config watch and start the server
+	if *bootstrap.Config.App.Debug {
+		watchConfig(app, bootstrap)
+	}
+
 	// setting up graceful shutdown
 	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
 	defer done()
 
 	ggroup, gctx := errgroup.WithContext(ctx)
-
-	// Channel to ensure graceful shutdown completes before restarting
-	shutdownComplete := make(chan struct{})
-	defer close(shutdownComplete)
-
-	var restartOngoing bool
-	// watch for config changes
-	if *bootstrap.Config.App.Debug {
-		viper.OnConfigChange(func(e fsnotify.Event) {
-			if restartOngoing {
-				return
-			}
-			restartOngoing = true
-			bootstrap.Logger.Info("Config changed:", zap.String("file", e.Name))
-			err := viper.Unmarshal(bootstrap.Config)
-			if err != nil {
-				bootstrap.Logger.Error("cannot unmarshall to config struct", zap.Error(err))
-				restartOngoing = false
-				return
-			}
-
-			err = bootstrap.Config.Validate()
-			if err != nil {
-				bootstrap.Logger.Error("cannot validating config:", zap.Error(err))
-				restartOngoing = false
-				return
-			}
-			if *bootstrap.Config.App.Verbose {
-				bootstrap.Config.Print(bootstrap.Logger)
-			}
-
-			go func() {
-				bootstrap.Logger.Info("shutting down server to apply new configuration")
-				if err := app.Shutdown(); err != nil {
-					bootstrap.Logger.Error("App fiber server forced to shutdown", zap.Error(err))
-					restartOngoing = false
-					return
-				}
-				shutdownComplete <- struct{}{}
-			}()
-
-			// Wait until shutdown is completed
-			<-shutdownComplete
-
-			bootstrap.Logger.Info("starting server with new configuration")
-			restartOngoing = false
-
-			if err := startServer(app, bootstrap); err != nil {
-				bootstrap.Logger.Error("Failed to restart server", zap.Error(err))
-			}
-
-		})
-
-		viper.WatchConfig()
-	}
 
 	// start the server on port in a separate goroutine
 	ggroup.Go(func() error {
@@ -120,5 +57,61 @@ func startServer(app *fiber.App, bootstrap Bootstrap) error {
 		return err
 	}
 
+	return nil
+}
+
+func watchConfig(app *fiber.App, bootstrap Bootstrap) error {
+	var restartOngoing bool
+
+	// channel to notify when the shutdown is complete
+	shutdownComplete := make(chan struct{})
+
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		if restartOngoing {
+			return
+		}
+		restartOngoing = true
+		bootstrap.Logger.Info("Config changed:", zap.String("file", e.Name))
+		err := viper.Unmarshal(bootstrap.Config)
+		if err != nil {
+			bootstrap.Logger.Error("cannot unmarshall to config struct", zap.Error(err))
+			restartOngoing = false
+			return
+		}
+
+		err = bootstrap.Config.Validate()
+		if err != nil {
+			bootstrap.Logger.Error("cannot validating config:", zap.Error(err))
+			restartOngoing = false
+			return
+		}
+		if *bootstrap.Config.App.Verbose {
+			bootstrap.Config.Print(bootstrap.Logger)
+		}
+
+		go func() {
+			bootstrap.Logger.Info("shutting down server to apply new configuration")
+			if err := app.Shutdown(); err != nil {
+				bootstrap.Logger.Error("App fiber server forced to shutdown", zap.Error(err))
+				restartOngoing = false
+				return
+			}
+			shutdownComplete <- struct{}{}
+		}()
+
+		// Wait until shutdown is completed
+		<-shutdownComplete
+
+		bootstrap.Logger.Info("starting server with new configuration")
+
+		newApp := startApp(bootstrap)
+
+		if err := runServer(newApp, bootstrap); err != nil {
+			bootstrap.Logger.Error("Failed to restart server", zap.Error(err))
+		}
+		restartOngoing = false
+	})
+
+	viper.WatchConfig()
 	return nil
 }
